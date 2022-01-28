e1293059f99ff90e287bcd9ca28901d0308c6795
Nghia Ta <nghiatt@bsscommerce.com>
2022-01-28 16:23:55 +0700

    delete target folder

--- contracts/nft-contract/target/release/build/serde-7c3951e5294c5be9/build_script_build-7c3951e5294c5be9.d
+++
@@ -1,5 +0,0 @@
-/home/nghiata/Workspace/Blockchain/near/near-hackathon-nft/contracts/nft-contract/target/release/build/serde-7c3951e5294c5be9/build_script_build-7c3951e5294c5be9: /home/nghiata/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.118/build.rs
-
-/home/nghiata/Workspace/Blockchain/near/near-hackathon-nft/contracts/nft-contract/target/release/build/serde-7c3951e5294c5be9/build_script_build-7c3951e5294c5be9.d: /home/nghiata/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.118/build.rs
-
-/home/nghiata/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.118/build.rs:
--- contracts/nft-contract/target/release/deps/borsh_derive-9716af4d796c49fb.d
+++
@@ -1,5 +0,0 @@
-/home/nghiata/Workspace/Blockchain/near/near-hackathon-nft/contracts/nft-contract/target/release/deps/libborsh_derive-9716af4d796c49fb.so: /home/nghiata/.cargo/registry/src/github.com-1ecc6299db9ec823/borsh-derive-0.8.1/src/lib.rs
-
-/home/nghiata/Workspace/Blockchain/near/near-hackathon-nft/contracts/nft-contract/target/release/deps/borsh_derive-9716af4d796c49fb.d: /home/nghiata/.cargo/registry/src/github.com-1ecc6299db9ec823/borsh-derive-0.8.1/src/lib.rs
-
-/home/nghiata/.cargo/registry/src/github.com-1ecc6299db9ec823/borsh-derive-0.8.1/src/lib.rs:
